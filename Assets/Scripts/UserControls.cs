7563c8887a308dd42a63a6ac2b9bb5d2c7bcb05a
ShiNoSenshi <darksenshi@gmail.com>
2017-09-23 01:18:57 +0200

    Game is now working on OnePlusOne

--- Assets/Scripts/UserControls.cs
+++ Assets/Scripts/UserControls.cs
@@ -13,8 +13,8 @@ public class UserControls : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        float horizontal = Input.GetAxis("Horizontal") * -1;
-        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.acceleration.x * -1;
+        float vertical = Input.acceleration.y;
         //Debug.Log("Horizontal = " + horizontal + "; Vertical = " + vertical);        
         Quaternion target = Quaternion.Euler(MAX_TILT * vertical, 0, MAX_TILT * horizontal);
         transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * SMOOTH);
